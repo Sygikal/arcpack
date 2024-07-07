2ac3fa5b8cfaeecf78a41014b7356295d0cba0d5
Sygikal <xtin8873@gmail.com>
2024-07-07 16:20:32 -0700

    add backpack to atlas

--- resources/assets/illagerexp/gen.js
+++
@@ -1,8 +0,0 @@
-for await (const f of Deno.readDir('textures/pbentity')) {
-	Deno.writeTextFileSync("models/pbentity/" + f.name.substring(0, f.name.length - 4) + ".json", JSON.stringify({
-		parent: "illagerexp:pbentity/generic_illager_head",
-		"textures": {
-			"0": "illagerexp:pbentity/" + f.name.substring(0, f.name.length - 4)
-		}
-	}))
-}
